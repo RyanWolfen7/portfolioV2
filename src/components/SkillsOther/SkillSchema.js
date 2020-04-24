902f1934f220959fa6c0986c58e16cf7d0c43d8d
RyanWolfen7 <wolfen7lada@gmail.com>
2020-04-23 21:51:49 -0500

    made an time complex schema for gigles

---
+++ src/components/SkillsOther/SkillSchema.js
@@ -0,0 +1,33 @@
+import skills from '../../assets/otherSkills/index'
+
+const { bow, cooking, fighting, guitar, infantry, photography, poetry, steam, survival, weightlifting } = skills
+
+const bowDetails = ['Bowyer', 'I can', bow, ['Make Bows', 'Fletch Arrows', 'Make Bow Strings', 'Shoot Bows'] ]
+const cookingDetails = ['Cook', 'I specialize in', cooking, ['Traditional Southeast Asian Cuisine', ' Traditional Mexican and Texmex', 'Grilling Meat', 'Gluten Free', 'Vegan Substitutions']]
+const fightingDetails = ['Martial Arts', 'I enjoy practicing', fighting, ['Jujitsu', 'Boxing', 'HEMA', 'MMA'] ]
+const guitarDetails = ['Singer Songwriter', 'I', guitar, ['Write Songs', 'Play Guitar', 'Sing'] ]
+const infantryDetails = ['Infantry Skills', `I'm an expert in`, infantry, ['Close Combat', 'Small Unit Tactics', 'Fire Arms', 'Jungle Warefare', 'Training', 'Leadership', 'First Aid']]
+const photographyDetails = ['Photography', 'I studied', photography, ['35mm Film', 'Black and White Film', 'Film Exposure', 'Printing']]
+const poetryDetails = ['Poetry', 'I like writting', poetry, ['Short Poems', 'Haikus', `Many of my poems are inspired by what I'm feeling in the moment`, 'https://hellopoetry.com/ryan-clark/']]
+const steamDetails = ['Video Games', 'I like', steam, [ 'Playing Them', 'Making Them', 'Modding Them']]
+const survivalDetails = ['Survival', `I'm an expert in`, survival, ['Inprovised Shelters', 'Hunting', 'Field First Aid', 'Orienteering']]
+
+const schemaBuider = skill => {
+    return { title: skill[0], subTitle: skill[1], image: skill[2], skillArray: skill[3] }
+}
+
+export default [
+    bowDetails,
+    cookingDetails,
+    fightingDetails,
+    guitarDetails,
+    infantryDetails,
+    photographyDetails,
+    poetryDetails,
+    steamDetails,
+    survivalDetails
+].reduce(( array, skill) => {
+    array.push( schemaBuider(skill) )
+    return array
+}, [])
+
--- src/components/SkillsOther/SkillsOther.js
+++ src/components/SkillsOther/SkillsOther.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import CardCarousel from '../CardCarousel/CardCarousel'
 import { OtherContainer } from '../../styles/SkillsViewStyles'
+import schema from './SkillSchema'
 
 const OtherSkills = props => {
-
+    console.log(schema)
     return (
         <OtherContainer>
         </OtherContainer>
