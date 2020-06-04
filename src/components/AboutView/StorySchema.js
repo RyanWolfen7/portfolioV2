c80e69ef6b288e75d0294d1dee271dd275e449ae
RyanWolfen7 <wolfen7lada@gmail.com>
2020-06-04 11:05:33 -0500

    fixed typo

--- src/components/AboutView/StorySchema.js
+++ src/components/AboutView/StorySchema.js
@@ -28,7 +28,7 @@ const myStory = (
             One of the pros to not having a sedentary home is that I had to get use to making new connections everywhere I've gone.</p>
         <p> In Highschool I lived in Clarksville, TN. There I worked for AT&T, broke horses for a summer, and helped in restoration projects during the TN floods. I have many a
             fond memories there and tons of adventures to boot.</p>
-        <p> After HS I moved to San Anotnio, TX. There I worked for AT&T as a door to door salesman, moonlighted as a bartender, and went to University of Texas San Antonio.
+        <p> After HS I moved to San Antonio, TX. There I worked for AT&T as a door to door salesman, moonlighted as a bartender, and went to University of Texas San Antonio.
             I loved Texas, but after two years in I was looking towards following my parents footsteps and enlisting in the US Army. I sold all of my things, took a break from school, put my notice into work and drove off to Ft. Walton Beach, Florida to train with my dads unit while I went through the enlistment process. </p>
     </>
 )
